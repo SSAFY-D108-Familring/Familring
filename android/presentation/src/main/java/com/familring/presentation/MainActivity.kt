7b5ec03168c83c13b39241bb395cc87838844533
HYUNJIN LEE <dlguswls456@naver.com>
2024-10-25 18:01:47 +0900

    feat : 타임캡슐 화면1 UI

--- android/presentation/src/main/java/com/familring/presentation/MainActivity.kt
+++ android/presentation/src/main/java/com/familring/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.familring.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.familring.presentation.screen.TimeCapsuleScreen
 import com.familring.presentation.theme.FamilringTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +12,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             FamilringTheme {
-                MainScreen()
+                TimeCapsuleScreen()
             }
         }
     }
