a909bca43a772b79b6012a3cdca8266243c8d51d
권오윤 <rdd2033@naver.com>
2024-10-28 16:25:20 +0900

    build : api-gateway, discovery, config service 세팅 완

--- backend/discovery-service/src/main/java/com/familring/discoveryservice/DiscoveryServiceApplication.java
+++ backend/discovery-service/src/main/java/com/familring/discoveryservice/DiscoveryServiceApplication.java
@@ -1,15 +1,16 @@
-package com.familring.userservice;
+package com.familring.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class UserServiceApplication {
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 
 }
