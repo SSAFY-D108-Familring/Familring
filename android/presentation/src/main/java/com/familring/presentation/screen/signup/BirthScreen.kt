cb65c4d23d13c2477862025c398c53e8d5b8e60c
NAKYUNG LEE <lnk12888@naver.com>
2024-10-28 15:20:18 +0900

    ui(#S11P31D108-193): 생년월일 입력 화면 UI 제작

--- android/presentation/src/main/java/com/familring/presentation/screen/signup/BirthScreen.kt
+++ android/presentation/src/main/java/com/familring/presentation/screen/signup/BirthScreen.kt
@@ -1,11 +1,106 @@
 package com.familring.presentation.screen.signup
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.familring.presentation.component.DateInputRow
+import com.familring.presentation.component.GreenRoundLongButton
+import com.familring.presentation.component.TopAppBar
+import com.familring.presentation.screen.timecapsule.isDateFormValid
+import com.familring.presentation.theme.Black
+import com.familring.presentation.theme.Gray01
+import com.familring.presentation.theme.Typography
 
 @Composable
-fun BirthScreen(modifier: Modifier = Modifier) {
+fun BirthRoute(
+    modifier: Modifier,
+    popUpBackStack: () -> Unit,
+    navigateToColor: () -> Unit,
+) {
+    BirthScreen(
+        modifier = modifier,
+        popUpBackStack = popUpBackStack,
+        navigateToColor = navigateToColor,
+    )
+}
+
+@Composable
+fun BirthScreen(
+    modifier: Modifier = Modifier,
+    popUpBackStack: () -> Unit = {},
+    navigateToColor: () -> Unit = {},
+) {
+    var year by remember { mutableStateOf("") }
+    var month by remember { mutableStateOf("") }
+    var date by remember { mutableStateOf("") }
+    val isButtonEnabled = isDateFormValid(year, month, date)
+
+    val focusManager = LocalFocusManager.current
+
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = Color.White,
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "생년월일 입력",
+                        color = Black,
+                        style = Typography.headlineMedium.copy(fontSize = 22.sp),
+                    )
+                },
+                onNavigationClick = popUpBackStack,
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            Text(
+                modifier = Modifier.padding(start = 20.dp),
+                text = "입력해 주시는 생년월일을 바탕으로",
+                style = Typography.bodyLarge,
+                color = Gray01,
+            )
+            Text(
+                modifier = Modifier.padding(start = 20.dp),
+                text = "귀여운 12간지 캐릭터 프로필을 만들어 드려요!",
+                style = Typography.bodyLarge,
+                color = Gray01,
+            )
+            Spacer(modifier = Modifier.fillMaxHeight(0.25f))
+            DateInputRow(
+                year = year,
+                month = month,
+                date = date,
+                onYearChange = { year = it },
+                onMonthChange = { month = it },
+                onDateChange = { date = it },
+                focusManager = focusManager,
+            )
+            Spacer(modifier = Modifier.fillMaxHeight(0.07f))
+            GreenRoundLongButton(
+                text = "다음으로",
+                onClick = navigateToColor,
+                enabled = isButtonEnabled,
+            )
+        }
+    }
 }
 
 @Composable
