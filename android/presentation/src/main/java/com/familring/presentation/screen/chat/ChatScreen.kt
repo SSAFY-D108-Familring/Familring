7a690ff0eae21f48ad1436c6bcb5876fd5bb6da7
nakyung128 <lnk12888@naver.com>
2024-10-28 23:40:11 +0900

    ui(#S11P31D108-191): 임시로 Home, Chat Screen 제작

---
+++ android/presentation/src/main/java/com/familring/presentation/screen/chat/ChatScreen.kt
@@ -0,0 +1,13 @@
+package com.familring.presentation.screen.chat
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChatRoute(modifier: Modifier) {
+    ChatScreen(modifier = modifier)
+}
+
+@Composable
+fun ChatScreen(modifier: Modifier = Modifier) {
+}
---
+++ android/presentation/src/main/java/com/familring/presentation/screen/home/HomeScreen.kt
@@ -0,0 +1,13 @@
+package com.familring.presentation.screen.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeRoute(modifier: Modifier) {
+    HomeScreen(modifier = modifier)
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+}
