12ae788c2381fe51d8bdece6a32fcf232a5b3e75
권오윤 <rdd2033@naver.com>
2024-10-28 17:35:38 +0900

    료refactor : user service 예외 수

---
+++ backend/user-service/src/main/java/com/familring/userservice/exception/base/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.luckycookie.crewin.dto.base;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@RequiredArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime timeStamp;
+    private String errorCode;
+    private String message;
+
+    public ErrorResponse(String errorCode, String message) {
+        this.timeStamp = LocalDateTime.now().withNano(0);
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+}
