a88950fbe00527b5a13c3405a62843cd60ef0624
윤가희 <yunkh2005@naver.com>
2024-10-30 12:57:37 +0900

    build: user-service

--- backend/user-service/src/main/java/com/familring/userservice/config/security/SecurityConfig.java
+++ backend/user-service/src/main/java/com/familring/userservice/config/security/SecurityConfig.java
@@ -33,7 +33,6 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-<<<<<<< HEAD
         http.httpBasic(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -42,20 +41,6 @@ public class SecurityConfig {
                                 .requestMatchers("/**").access(this::hasIpAddresses)
                                 .anyRequest().authenticated())
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
-=======
-        http
-                .httpBasic(httpConfig -> httpConfig.disable()) // 기본 인증을 비활성화 (HTTP Basic Authentication)
-                .csrf(csrfConfig -> csrfConfig.disable()) // CSRF 보호를 비활성화 (Cross-Site Request Forgery)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 관리를 Stateless로 설정 (서버에 세션을 저장하지 않음)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**").permitAll() // Swagger UI 접근 허용
-                        .requestMatchers("/v3/api-docs/**").permitAll() // API 문서 접근 허용
-                        .requestMatchers("/users/join/**", "/users/login/**").permitAll() // 회원가입, 로그인, 토큰 재발급 접근 허용
-                        .requestMatchers("/test/**").permitAll() // 테스트 접근 허용
-                        .anyRequest().authenticated() // 나머지 요청은 인증 필요
-                )
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class); // JWT 필터를 UsernamePasswordAuthenticationFilter 앞에 추가
->>>>>>> feature/backend/family
 
         return http.build();
     }
