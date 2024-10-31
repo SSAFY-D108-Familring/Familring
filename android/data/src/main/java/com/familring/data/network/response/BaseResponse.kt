542e21b9d0e2d9322bf299f89cef0f79f3d0e05a
NAKYUNG LEE <lnk12888@naver.com>
2024-10-31 15:07:35 +0900

    feat(#S11P31D108-264): Response 작성

---
+++ android/data/src/main/java/com/familring/data/network/response/BaseResponse.kt
@@ -0,0 +1,49 @@
+package com.familring.data.network.response
+
+import com.familring.data.exception.ApiException
+import com.familring.data.exception.RefreshTokenExpiredException
+import com.familring.domain.model.ApiResponse
+import java.io.IOException
+
+data class BaseResponse<T>(
+    val code: String,
+    val data: T?,
+    val message: String,
+)
+
+suspend fun <T> emitApiResponse(
+    apiResponse: suspend () -> BaseResponse<T>,
+    default: T,
+): ApiResponse<T> =
+    runCatching {
+        apiResponse()
+    }.fold(
+        onSuccess = { result ->
+            ApiResponse.Success(data = result.data ?: default)
+        },
+        onFailure = { e ->
+            when (e) {
+                is ApiException ->
+                    ApiResponse.Error.ServerError(
+                        errorCode = e.error.errorCode,
+                        errorMessage = e.error.message,
+                    )
+
+                is RefreshTokenExpiredException ->
+                    ApiResponse.Error.TokenError(
+                        errorCode = e.error.errorCode,
+                        errorMessage = e.error.message,
+                    )
+
+                is IOException ->
+                    ApiResponse.Error.NetworkError(
+                        errorMessage = e.message ?: "Network Error",
+                    )
+
+                else ->
+                    ApiResponse.Error.UnknownError(
+                        errorMessage = e.message ?: "Unknown Error",
+                    )
+            }
+        },
+    )
