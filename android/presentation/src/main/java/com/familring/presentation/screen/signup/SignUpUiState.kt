7967ab454169c73bb36badbdc8446e3d42d1078e
NAKYUNG LEE <lnk12888@naver.com>
2024-11-01 17:48:32 +0900

    feat(#S11P31D108-266): 회원가입 viewModel, UiEvent, UiState 생성

---
+++ android/presentation/src/main/java/com/familring/presentation/screen/signup/SignUpUiState.kt
@@ -0,0 +1,13 @@
+package com.familring.presentation.screen.signup
+
+import java.io.File
+
+data class SignUpUiState(
+    val make: Boolean = true,
+    val userKakaoId: String = "",
+    val userNickname: String = "",
+    val userBirthDate: String = "",
+    val userRole: String = "",
+    val userColor: String = "",
+    val userFace: File? = null,
+)
