8bae56d9075a473fc1650f6134fd11ec0640eed8
권오윤 <rdd2033@naver.com>
2024-11-01 20:24:11 +0900

    Reapply "build: file service 예외처리방식 팩토리 메서드로 변경"
    This reverts commit b0e03539cabc28ac20f9b806b520f9cbfd71710d.

--- backend/file-service/src/main/java/com/familring/fileservice/exception/S3Exception.java
+++ backend/file-service/src/main/java/com/familring/fileservice/exception/S3Exception.java
@@ -1,13 +1,39 @@
 package com.familring.fileservice.exception;
 
 import com.familring.fileservice.exception.base.ApplicationException;
-import lombok.Getter;
-import org.springframework.http.HttpStatus;
+import com.familring.fileservice.exception.constant.ErrorDetail;
 
-@Getter
 public class S3Exception extends ApplicationException {
-    public S3Exception(HttpStatus httpStatus, String message) {
-        super(httpStatus, message);
+    public S3Exception(ErrorDetail errorDetail) {
+        super(errorDetail);
     }
 
+    // 자주 사용되는 예외들에 대한 팩토리 메서드
+    public static S3Exception fileUploadError() {
+        return new S3Exception(ErrorDetail.S3_FILE_UPLOAD_ERROR);
+    }
+
+    public static S3Exception fileDeleteError() {
+        return new S3Exception(ErrorDetail.S3_FILE_DELETE_ERROR);
+    }
+
+    public static S3Exception fileNotFound(String fileKey) {
+        return new S3Exception(ErrorDetail.S3_FILE_NOT_FOUND);
+    }
+
+    public static S3Exception noFilesToUpload() {
+        return new S3Exception(ErrorDetail.S3_NO_FILES_TO_UPLOAD);
+    }
+
+    public static S3Exception noFilesUploaded() {
+        return new S3Exception(ErrorDetail.S3_NO_FILES_UPLOADED);
+    }
+
+    public static S3Exception noUrlsToDelete() {
+        return new S3Exception(ErrorDetail.S3_NO_URLS_TO_DELETE);
+    }
+
+    public static S3Exception invalidFileUrl() {
+        return new S3Exception(ErrorDetail.S3_INVALID_FILE_URL);
+    }
 }
