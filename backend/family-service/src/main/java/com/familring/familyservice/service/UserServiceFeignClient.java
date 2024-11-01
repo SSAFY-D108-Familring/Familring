2387bb1396aac32e06165ce25ed641b39bea77a0
윤가희 <yunkh2005@naver.com>
2024-11-01 13:34:41 +0900

    fix: 가족 구성원 조회 Feign Client 반환형 변경

--- backend/family-service/src/main/java/com/familring/familyservice/service/UserServiceFeignClient.java
+++ backend/family-service/src/main/java/com/familring/familyservice/service/UserServiceFeignClient.java
@@ -1,5 +1,6 @@
 package com.familring.familyservice.service;
 
+import com.familring.common_service.dto.BaseResponse;
 import com.familring.familyservice.model.dto.response.UserInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,6 @@ import java.util.List;
 
 @FeignClient(name = "user-service")
 public interface UserServiceFeignClient {
-    @PostMapping("info")
-    List<UserInfoResponse> getAllUser(@RequestBody List<Long> userIds);
+    @PostMapping("/users/info")
+    BaseResponse<List<UserInfoResponse>> getAllUser(@RequestBody List<Long> userIds);
 }
