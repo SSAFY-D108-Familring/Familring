62fd2820393370151a3ebfd44840c67f84349b5a
윤가희 <yunkh2005@naver.com>
2024-11-01 09:25:24 +0900

    chore: Swagger Operation 추가

--- backend/family-service/src/main/java/com/familring/familyservice/controller/FamilyController.java
+++ backend/family-service/src/main/java/com/familring/familyservice/controller/FamilyController.java
@@ -5,6 +5,7 @@ import com.familring.familyservice.model.dto.request.FamilyJoinRequest;
 import com.familring.familyservice.model.dto.response.FamilyInfoResponse;
 import com.familring.familyservice.model.dto.response.UserInfoResponse;
 import com.familring.familyservice.service.FamilyService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +26,7 @@ public class FamilyController {
     private final FamilyService familyService;
 
     @GetMapping
+    @Operation(summary = "회원의 가족 정보 조회 - Header", description = "Header의 토큰을 사용해 회원의 가족 정보를 조회")
     public ResponseEntity getFamilyInfo(@Parameter(hidden = true) @RequestHeader("X-User-ID") Long userId) {
         FamilyInfoResponse response = familyService.getFamilyInfo(userId);
 
@@ -32,6 +34,7 @@ public class FamilyController {
     }
 
     @GetMapping("/code")
+    @Operation(summary = "가족 코드 조회", description = "Header의 토큰을 사용해 회원의 가족 정보 중 가족 코드를 조회")
     public ResponseEntity getFamilyCode(@Parameter(hidden = true) @RequestHeader("X-User-ID") Long userId) {
         String response = familyService.getFamilyCode(userId);
 
@@ -39,6 +42,7 @@ public class FamilyController {
     }
 
     @GetMapping("/member")
+    @Operation(summary = "가족 구성원 조회", description = "Header의 토큰을 사용해 회원의 가족 정보 중 가족 코드를 조회")
     public ResponseEntity getFamilyMemberList(@Parameter(hidden = true) @RequestHeader("X-User-Id") Long userId) {
         List<UserInfoResponse> responseList = familyService.getFamilyMemberList(userId);
 
@@ -46,6 +50,7 @@ public class FamilyController {
     }
 
     @PostMapping
+    @Operation(summary = "가족 생성", description = "가족 테이블을 생성")
     public ResponseEntity createFamily
             (@Parameter(hidden = true) @RequestHeader("X-User-Id") Long userId,
              @RequestBody FamilyCreateRequest familyCreateRequest) {
@@ -55,6 +60,7 @@ public class FamilyController {
     }
 
     @PostMapping("/join")
+    @Operation(summary = "가족 구성원 추가", description = "가족코드에 해당하는 가족에 가족 구성원 추가")
     public ResponseEntity joinFamilyMember
             (@Parameter(hidden = true) @RequestHeader("X-User-Id") Long userId,
              @RequestBody FamilyJoinRequest familyJoinRequest) {
@@ -64,6 +70,7 @@ public class FamilyController {
     }
 
     @PatchMapping("/member")
+    @Operation(summary = "가족 구성원 제거", description = "가족 구성원에서 Header의 토큰에 해당하는 회원 삭제")
     public ResponseEntity deleteFamilyMember (@Parameter(hidden = true) @RequestHeader("X-User-Id") Long userId) {
         String response = familyService.deleteFamilyMember(userId);
 
