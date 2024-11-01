472e1751b3636aeda6ac213529f413a0eca5cd8b
윤가희 <yunkh2005@naver.com>
2024-11-01 15:42:53 +0900

    feat: MSA 전용 가족 구성원 조회 API 추가

--- backend/family-service/src/main/java/com/familring/familyservice/controller/client/FamilyClientController.java
+++ backend/family-service/src/main/java/com/familring/familyservice/controller/client/FamilyClientController.java
@@ -1,7 +1,8 @@
-package com.familring.familyservice.controller;
+package com.familring.familyservice.controller.client;
 
 import com.familring.common_service.dto.BaseResponse;
 import com.familring.familyservice.model.dto.response.FamilyInfoResponse;
+import com.familring.familyservice.model.dto.response.UserInfoResponse;
 import com.familring.familyservice.service.FamilyService;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,20 +16,28 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/client/family")
 @RequiredArgsConstructor
 @Log4j2
 @Hidden
-public class FamilyMsaController {
+public class FamilyClientController {
 
     private final FamilyService familyService;
 
     @GetMapping
-    @Operation(summary = "회원의 가족 정보 조회 - Header", description = "Header의 토큰을 사용해 회원의 가족 정보를 조회")
-    public ResponseEntity<BaseResponse<FamilyInfoResponse>> getFamilyInfo(@Parameter(hidden = true) @RequestHeader("X-User-ID") Long userId) {
+    public ResponseEntity<BaseResponse<FamilyInfoResponse>> getFamilyInfo(Long userId) {
         FamilyInfoResponse response = familyService.getFamilyInfo(userId);
 
         return ResponseEntity.ok(BaseResponse.create(HttpStatus.OK.value(), "가족 정보를 성공적으로 조회 했습니다.", response));
+    }
+
+    @GetMapping("/member")
+    public ResponseEntity<BaseResponse<List<UserInfoResponse>>> getFamilyMemberList(Long userId) {
+        List<UserInfoResponse> responseList = familyService.getFamilyMemberList(userId);
+
+        return ResponseEntity.ok(BaseResponse.create(HttpStatus.OK.value(), "가족 구성원들을 성공적으로 조회 했습니다.", responseList));
     }
 }
