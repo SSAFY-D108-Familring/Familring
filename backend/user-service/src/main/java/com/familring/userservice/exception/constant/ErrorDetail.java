8f1d754aec1d7f0b576210cc019129913741abf2
권오윤 <rdd2033@naver.com>
2024-11-01 21:31:11 +0900

    fix: User 에러 코드 변경

--- backend/user-service/src/main/java/com/familring/userservice/exception/constant/ErrorDetail.java
+++ backend/user-service/src/main/java/com/familring/userservice/exception/constant/ErrorDetail.java
@@ -8,8 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorDetail {
     // User
-    INVALID_CREDENTIALS("U001", HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호가 올바르지 않습니다."),
-    NO_CONTENT_USER_IMAGE("U002", HttpStatus.BAD_REQUEST, "이미지가 존재하지 않습니다."),
+    INVALID_CREDENTIALS("U0001", HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호가 올바르지 않습니다."),
+    NO_CONTENT_USER_IMAGE("U0002", HttpStatus.BAD_REQUEST, "이미지가 존재하지 않습니다."),
 
     //Token
     EMPTY_AUTHORIZATION_HEADER("T0001", HttpStatus.UNAUTHORIZED, "인증 헤더가 존재하지 않습니다."),
