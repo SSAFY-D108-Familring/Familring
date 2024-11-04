dea7aa166b394e6fccbdf6594a2c72b3f232279f
김민경 <org9899@naver.com>
2024-11-04 16:37:41 +0900

    feat : 상태관리 Response 생성

--- backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/dto/response/TimeCapsuleStatusResponse.java
+++ backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/dto/response/TimeCapsuleStatusResponse.java
@@ -1,2 +1,21 @@
-package com.familring.timecapsuleservice.dto.response;public class TimeCapsuleStatusResponse {
+package com.familring.timecapsuleservice.dto.response;
+
+import com.familring.timecapsuleservice.dto.client.UserInfoResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TimeCapsuleStatusResponse {
+
+    private int status;
+    private int dayCount; // 남은 날짜
+    private Long count; // 몇 번째 타임캡슐 인지
+    private List<UserInfoResponse> users; // 작성한 가족 구성원 목록
+
 }
