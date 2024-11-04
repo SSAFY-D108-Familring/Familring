4701e06c96cda2f0a41d33ecd4213c92ee0eaa63
김민경 <org9899@naver.com>
2024-11-04 20:02:03 +0900

    feat : 타임캡슐 생성할 때 이미 존재하면 예외 처리

--- backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/exception/AlreadyExistTimeCapsuleException.java
+++ backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/exception/AlreadyExistTimeCapsuleException.java
@@ -1,2 +1,11 @@
-package com.familring.timecapsuleservice.exception;public class AlreadyExistTimeCapsuleException {
+package com.familring.timecapsuleservice.exception;
+
+import com.familring.timecapsuleservice.exception.base.ApplicationException;
+import com.familring.timecapsuleservice.exception.constant.ErrorDetail;
+
+public class AlreadyExistTimeCapsuleException extends ApplicationException {
+
+    public AlreadyExistTimeCapsuleException() {
+        super(ErrorDetail.EXIST_TIME_CAPSULE);
+    }
 }
--- backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/exception/constant/ErrorDetail.java
+++ backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/exception/constant/ErrorDetail.java
@@ -2,6 +2,7 @@ package com.familring.timecapsuleservice.exception.constant;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties;
 import org.springframework.http.HttpStatus;
 
 @Getter
@@ -9,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorDetail {
 
     NOT_FOUND_FAMILY("F0001", HttpStatus.NOT_FOUND, "가족을 찾지 못했습니다."),
-    NOT_FOUND_TIME_CAPSULE("T0001", HttpStatus.NOT_FOUND, "타입캡슐을 찾지 못했습니다.");
+    NOT_FOUND_TIME_CAPSULE("T0001", HttpStatus.NOT_FOUND, "타입캡슐을 찾지 못했습니다."),
+    EXIST_TIME_CAPSULE("T0002", HttpStatus.CONFLICT, "작성 중인 타임캡슐이 있습니다.");
 
     private final String errorCode;
     private final HttpStatus httpStatus;
