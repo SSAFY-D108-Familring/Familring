5ad7e1322cc3af1d1888178f5c073e0a515bc8d4
김민경 <org9899@naver.com>
2024-11-04 16:32:44 +0900

    feat : 타임캡슐, 타임캡슐 답변 entity 생성

--- backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/domain/TimeCapsule.java
+++ backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/domain/TimeCapsule.java
@@ -1,2 +1,41 @@
-package com.familring.timecapsuleservice.domain;public class Timecapsule {
+package com.familring.timecapsuleservice.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicInsert
+@Table(name = "timecapsule")
+public class TimeCapsule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="timecapsule_id")
+    private Long id;
+
+    @Column(name="family_id")
+    private Long familyId;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name="timecapsule_start_date")
+    private LocalDateTime startDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name="timecapsule_end_date")
+    private LocalDateTime endDate;
+
 }
