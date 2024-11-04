35ae96adacc0855d6059d3f8b978e602f61edcfc
권오윤 <rdd2033@naver.com>
2024-11-04 16:12:15 +0900

    fix: feignClient에 requestParam 추가

--- backend/calendar-service/src/main/java/com/familring/calendarservice/service/client/FamilyServiceFeignClient.java
+++ backend/calendar-service/src/main/java/com/familring/calendarservice/service/client/FamilyServiceFeignClient.java
@@ -5,6 +5,7 @@ import com.familring.calendarservice.dto.client.UserInfoResponse;
 import com.familring.common_service.dto.BaseResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -12,11 +13,11 @@ import java.util.List;
 public interface FamilyServiceFeignClient {
     // 가족의 정보를 조회
     @GetMapping("/client/family")
-    BaseResponse<FamilyInfoResponse> getFamilyInfo(Long userId);
+    BaseResponse<FamilyInfoResponse> getFamilyInfo(@RequestParam Long userId);
 
     // 가족 구성원들을 조회
     @GetMapping("/client/family/member")
-    BaseResponse<List<UserInfoResponse>> getFamilyMembers(Long userId);
+    BaseResponse<List<UserInfoResponse>> getFamilyMembers(@RequestParam Long userId);
 }
 
 
