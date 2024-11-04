03312785d57f472bb86612e201752e6f12c0610b
김민경 <org9899@naver.com>
2024-11-04 16:33:58 +0900

    feat : 타임캡슐, 타임캡슐 답변 레포지토리 추가

--- backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/repository/TimeCapsuleAnswerRepository.java
+++ backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/repository/TimeCapsuleAnswerRepository.java
@@ -1,12 +1,13 @@
 package com.familring.timecapsuleservice.repository;
 
-import com.familring.timecapsuleservice.domain.TimecapsuleAnswer;
+import com.familring.timecapsuleservice.domain.TimeCapsule;
+import com.familring.timecapsuleservice.domain.TimeCapsuleAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface TimecapsuleAnwerRepository extends JpaRepository<TimecapsuleAnswer, Integer> {
-    Optional<TimecapsuleAnswer> findTimecapsuleAnswerByFamilyIdAndTimeCapsuleId(Integer familyId, Integer timeCapsuleId);
+public interface TimeCapsuleAnswerRepository extends JpaRepository<TimeCapsuleAnswer, Integer> {
+    Optional<TimeCapsuleAnswer> getTimeCapsuleAnswerByUserIdAndTimecapsule(Long userId, TimeCapsule timeCapsule);
 }
--- backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/repository/TimeCapsuleRepository.java
+++ backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/repository/TimeCapsuleRepository.java
@@ -1,2 +1,18 @@
-package com.familring.timecapsuleservice.repository;public interface TimecapsuleRepository {
+package com.familring.timecapsuleservice.repository;
+
+import com.familring.timecapsuleservice.domain.TimeCapsule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+public interface TimeCapsuleRepository extends JpaRepository<TimeCapsule, Integer> {
+
+    @Query("SELECT t FROM TimeCapsule t WHERE t.startDate <= :currentDate AND t.endDate >= :currentDate AND t.familyId = :familyId")
+    Optional<TimeCapsule> findTimeCapsuleWithinDateRangeAndFamilyId(@Param("currentDate") LocalDateTime currentDate, @Param("familyId") Long familyId);
+
 }
