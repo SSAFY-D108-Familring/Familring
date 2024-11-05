12343fb83cd61cd0866cbe34550c32b0e570418f
nakyung128 <lnk12888@naver.com>
2024-11-05 23:15:47 +0900

    feat(#S11P31D108-347): 회원 정보 불러오는 함수 작성

--- android/presentation/src/main/java/com/familring/presentation/screen/mypage/MyPageViewModel.kt
+++ android/presentation/src/main/java/com/familring/presentation/screen/mypage/MyPageViewModel.kt
@@ -3,11 +3,15 @@ package com.familring.presentation.screen.mypage
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.familring.domain.model.ApiResponse
+import com.familring.domain.repository.FamilyRepository
 import com.familring.domain.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,21 +20,95 @@ class MyPageViewModel
     @Inject
     constructor(
         private val userRepository: UserRepository,
+        private val familyRepository: FamilyRepository,
     ) : ViewModel() {
-        private val _event = MutableSharedFlow<MyPageState>()
+        private val _state = MutableStateFlow<MyPageUiState>(MyPageUiState.Loading)
+        val state = _state.asStateFlow()
+
+        private val _event = MutableSharedFlow<MyPageUiEvent>()
         val event = _event.asSharedFlow()
+
+        init {
+            getMyInfo()
+        }
 
         fun signOut() {
             viewModelScope.launch {
                 userRepository.signOut().collectLatest { response ->
                     when (response) {
                         is ApiResponse.Success -> {
-                            _event.emit(MyPageState.Success)
+                            _event.emit(MyPageUiEvent.SignOutSuccess)
                         }
 
                         is ApiResponse.Error -> {
-                            _event.emit(MyPageState.Error(response.code, response.message))
+                            _event.emit(MyPageUiEvent.Error(response.code, response.message))
                         }
+                    }
+                }
+            }
+        }
+
+        private fun getMyInfo() {
+            viewModelScope.launch {
+                combine(
+                    familyRepository.getFamilyCode(),
+                    userRepository.getUser(),
+                ) { familyResponse, userResponse ->
+                    var currentState = MyPageUiState.Success()
+                    currentState =
+                        when (userResponse) {
+                            is ApiResponse.Success -> {
+                                val data = userResponse.data
+                                currentState.copy(
+                                    userNickname = data.userNickname,
+                                    userColor = data.userColor,
+                                    userRole = data.userRole,
+                                    userBirthDate = data.userBirthDate.toString(),
+                                    profileImage = data.userZodiacSign,
+                                )
+                            }
+
+                            is ApiResponse.Error -> {
+                                _event.emit(
+                                    MyPageUiEvent.Error(
+                                        userResponse.code,
+                                        userResponse.message,
+                                    ),
+                                )
+                                currentState
+                            }
+                        }
+                    currentState =
+                        when (familyResponse) {
+                            is ApiResponse.Success -> {
+                                currentState.copy(code = familyResponse.data)
+                            }
+
+                            is ApiResponse.Error -> {
+                                _event.emit(
+                                    MyPageUiEvent.Error(
+                                        familyResponse.code,
+                                        familyResponse.message,
+                                    ),
+                                )
+                                currentState
+                            }
+                        }
+                    currentState
+                }.collectLatest { updateState ->
+                    val state = _state.value
+                    if (state is MyPageUiState.Loading) {
+                        _state.value = updateState
+                    } else if (state is MyPageUiState.Success) {
+                        _state.value =
+                            state.copy(
+                                userNickname = updateState.userNickname,
+                                userColor = updateState.userColor,
+                                userRole = updateState.userRole,
+                                userBirthDate = updateState.userBirthDate,
+                                profileImage = updateState.profileImage,
+                                code = updateState.code,
+                            )
                     }
                 }
             }
