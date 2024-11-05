251e42202b4b13d7ae51ad6a0d9a50f6a919ec6c
김민경 <org9899@naver.com>
2024-11-06 01:31:34 +0900

    feat : 질문 수정 dto 추가

--- backend/question-service/src/main/java/com/familring/questionservice/dto/request/QuestionAnswerUpdateRequest.java
+++ backend/question-service/src/main/java/com/familring/questionservice/dto/request/QuestionAnswerUpdateRequest.java
@@ -1,2 +1,19 @@
-package com.familring.questionservice.dto.request;public class QuestionAnswerUpdateRequest {
+package com.familring.questionservice.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuestionAnswerUpdateRequest {
+
+    private String content;
+    private LocalDate modifiedAt;
+
 }
