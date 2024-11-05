6e42a6fe08521abc976d2daee0a108241ade4dd0
김민경 <org9899@naver.com>
2024-11-06 01:32:28 +0900

    feat : answerId로 사용자가 작성한 답변 조회 함수 추가

--- backend/question-service/src/main/java/com/familring/questionservice/repository/QuestionAnswerRepository.java
+++ backend/question-service/src/main/java/com/familring/questionservice/repository/QuestionAnswerRepository.java
@@ -4,10 +4,14 @@ import com.familring.questionservice.domain.QuestionAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface QuestionAnswerRepository extends JpaRepository<QuestionAnswer, Integer> {
 
     boolean existsByQuestionFamilyIdAndUserId(Long questionFamilyId, Long userId);
+
+    Optional<QuestionAnswer> findById(Long id);
 
 }
