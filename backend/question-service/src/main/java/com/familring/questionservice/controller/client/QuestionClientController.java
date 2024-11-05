aa578366d9cb569da2fdd5c8cce7a4faadeacfa5
김민경 <org9899@naver.com>
2024-11-05 15:09:56 +0900

    feat : 랜덤 질문 Client 생성 (Family Service 에서 사용)

--- backend/question-service/src/main/java/com/familring/questionservice/controller/client/QuestionClientController.java
+++ backend/question-service/src/main/java/com/familring/questionservice/controller/client/QuestionClientController.java
@@ -1,8 +1,23 @@
 package com.familring.questionservice.controller.client;
 
+import com.familring.common_module.dto.BaseResponse;
+import com.familring.questionservice.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-public class QuestinoClientController {
+@RestController
+@RequestMapping("/client/questions")
+@RequiredArgsConstructor
+public class QuestionClientController {
 
-    
+    private final QuestionService questionService;
+
+    @PostMapping()
+    public ResponseEntity<BaseResponse<Void>> initializeQuestionFamily(@RequestBody Long familyId) {
+        questionService.initializeQuestionFamily(familyId);
+        return ResponseEntity.ok(BaseResponse.create(HttpStatus.OK.value(), "첫 번째 랜덤 질문 생성에 성공했습니다."));
+    }
 
 }
