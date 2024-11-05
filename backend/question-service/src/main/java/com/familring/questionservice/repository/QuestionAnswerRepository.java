f19faeeb417702dc5e832aa37a716f03c039944b
김민경 <org9899@naver.com>
2024-11-06 00:07:55 +0900

    fix : 이미 있는 답변 찾아오는 함수 수정

--- backend/question-service/src/main/java/com/familring/questionservice/repository/QuestionAnswerRepository.java
+++ backend/question-service/src/main/java/com/familring/questionservice/repository/QuestionAnswerRepository.java
@@ -4,6 +4,7 @@ import com.familring.questionservice.domain.QuestionAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface QuestionAnswerRepository extends JpaRepository<QuestionAnswer, Integer> {
 
