48cfb48ed3f5532213e047c23d30b46bca82235b
김민경 <org9899@naver.com>
2024-11-05 20:18:31 +0900

    feat : 가족 질문 Not Found Exception 추가

--- backend/question-service/src/main/java/com/familring/questionservice/exception/QuestionFamilyNotFoundException.java
+++ backend/question-service/src/main/java/com/familring/questionservice/exception/QuestionFamilyNotFoundException.java
@@ -1,2 +1,10 @@
-package com.familring.questionservice.exception.constant;public class QuestionFamilyNotFoundException {
+package com.familring.questionservice.exception;
+
+import com.familring.questionservice.exception.base.ApplicationException;
+import com.familring.questionservice.exception.constant.ErrorDetail;
+
+public class QuestionFamilyNotFoundException extends ApplicationException {
+    public QuestionFamilyNotFoundException() {
+        super(ErrorDetail.NOT_FOUND_QUESTION_FAMILY);
+    }
 }
--- backend/question-service/src/main/java/com/familring/questionservice/exception/constant/ErrorDetail.java
+++ backend/question-service/src/main/java/com/familring/questionservice/exception/constant/ErrorDetail.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorDetail {
 
-    NOT_FOUND_QUESTION("Q0001", HttpStatus.NOT_FOUND, "질문을 찾지 못했습니다.");
+    NOT_FOUND_QUESTION("Q0001", HttpStatus.NOT_FOUND, "질문을 찾지 못했습니다."),
+    NOT_FOUND_QUESTION_FAMILY("Q0002", HttpStatus.NOT_FOUND, "가족의 질문을 찾지 못했습니다.");
 
     private final String errorCode;
     private final HttpStatus httpStatus;
