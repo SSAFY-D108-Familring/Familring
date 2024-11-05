ff4f7c828edf779a9902d056b862e3969d9c2abb
김민경 <org9899@naver.com>
2024-11-06 02:12:29 +0900

    feat : questionFamily랑 userID로 질문 답변 찾기 추가

--- backend/question-service/src/main/java/com/familring/questionservice/repository/QuestionAnswerRepository.java
+++ backend/question-service/src/main/java/com/familring/questionservice/repository/QuestionAnswerRepository.java
@@ -1,6 +1,7 @@
 package com.familring.questionservice.repository;
 
 import com.familring.questionservice.domain.QuestionAnswer;
+import com.familring.questionservice.domain.QuestionFamily;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,5 +14,7 @@ public interface QuestionAnswerRepository extends JpaRepository<QuestionAnswer, 
     boolean existsByQuestionFamilyIdAndUserId(Long questionFamilyId, Long userId);
 
     Optional<QuestionAnswer> findById(Long id);
+
+    Optional<QuestionAnswer> findByQuestionFamilyAndUserId(QuestionFamily questionFamily, Long userId);
 
 }
--- backend/question-service/src/main/java/com/familring/questionservice/repository/QuestionRepository.java
+++ backend/question-service/src/main/java/com/familring/questionservice/repository/QuestionRepository.java
@@ -4,6 +4,8 @@ import com.familring.questionservice.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
