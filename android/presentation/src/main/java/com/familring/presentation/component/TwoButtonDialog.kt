cb7829627c6316190509983334ac9d1bdd10319f
NAKYUNG LEE <lnk12888@naver.com>
2024-11-05 15:28:27 +0900

    feat(#S11P31D108-346): 회원탈퇴 기능 구현 완료

--- android/presentation/src/main/java/com/familring/presentation/component/TwoButtonDialog.kt
+++ android/presentation/src/main/java/com/familring/presentation/component/TwoButtonDialog.kt
@@ -4,10 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
@@ -58,6 +58,7 @@ fun TwoButtonTextDialog(
             modifier =
                 Modifier
                     .fillMaxWidth()
+                    .height(IntrinsicSize.Min)
                     .clip(RoundedCornerShape(bottomStart = 12.dp, bottomEnd = 12.dp))
                     .background(color = Green02),
             contentAlignment = Alignment.Center,
@@ -65,14 +66,13 @@ fun TwoButtonTextDialog(
             Row(
                 modifier =
                     Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(0.28f),
+                        .fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically,
             ) {
                 Box(
                     modifier =
                         Modifier
-                            .fillMaxSize()
+                            .padding(vertical = 16.dp)
                             .weight(1f)
                             .clickable { onDismissClick() },
                     contentAlignment = Alignment.Center,
@@ -91,7 +91,7 @@ fun TwoButtonTextDialog(
                 Box(
                     modifier =
                         Modifier
-                            .fillMaxSize()
+                            .padding(vertical = 16.dp)
                             .weight(1f)
                             .clickable { onConfirmClick() },
                     contentAlignment = Alignment.Center,
