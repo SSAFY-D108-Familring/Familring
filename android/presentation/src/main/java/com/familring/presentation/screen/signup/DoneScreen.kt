e6b036ef3323d80bc07c03f8d1554a5ad2bcd549
NAKYUNG LEE <lnk12888@naver.com>
2024-11-06 16:37:24 +0900

    feat(#S11P31D108-342): shareCode 위치 변경

--- android/presentation/src/main/java/com/familring/presentation/screen/signup/DoneScreen.kt
+++ android/presentation/src/main/java/com/familring/presentation/screen/signup/DoneScreen.kt
@@ -1,5 +1,7 @@
 package com.familring.presentation.screen.signup
 
+import android.content.ActivityNotFoundException
+import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -29,11 +31,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.familring.presentation.R
 import com.familring.presentation.component.BrownRoundButton
 import com.familring.presentation.component.LoadingDialog
-import com.familring.presentation.screen.mypage.shareCode
 import com.familring.presentation.theme.Black
 import com.familring.presentation.theme.Gray01
 import com.familring.presentation.theme.Gray02
@@ -42,7 +44,15 @@ import com.familring.presentation.theme.Green03
 import com.familring.presentation.theme.Typography
 import com.familring.presentation.theme.White
 import com.familring.presentation.util.noRippleClickable
+import com.kakao.sdk.common.util.KakaoCustomTabsClient
+import com.kakao.sdk.share.ShareClient
+import com.kakao.sdk.share.WebSharerClient
+import com.kakao.sdk.template.model.Button
+import com.kakao.sdk.template.model.Content
+import com.kakao.sdk.template.model.FeedTemplate
+import com.kakao.sdk.template.model.Link
 import kotlinx.coroutines.flow.collectLatest
+import timber.log.Timber
 
 @Composable
 fun DoneRoute(
@@ -174,6 +184,69 @@ fun DoneScreen(
                 color = Gray01,
                 textDecoration = TextDecoration.Underline,
             )
+        }
+    }
+}
+
+fun shareCode(
+    code: String,
+    context: Context,
+    showSnackBar: (String) -> Unit,
+) {
+    val codeMessage =
+        FeedTemplate(
+            content =
+                Content(
+                    title = "가족에게서 코드가 도착했어요! $code",
+                    description = "패밀링에서 코드를 입력하고, 가족과 소통해 보세요.",
+                    imageUrl = "https://contents.creators.mypetlife.co.kr/content/uploads/2020/03/19135734/thumb-20200319125759_851795890399d09add3c23661fc38b19_pocq_700x524.png",
+                    link = Link(),
+                ),
+            buttons =
+                listOf(
+                    Button(
+                        "앱 실행하고 코드 복사하기",
+                        Link(
+                            androidExecutionParams = mapOf("action" to "copy_code", "code" to code),
+                        ),
+                    ),
+                ),
+        )
+
+    if (ShareClient.instance.isKakaoTalkSharingAvailable(context)) {
+        // 카카오톡으로 공유 가능
+        ShareClient.instance.shareDefault(context, codeMessage) { result, error ->
+            if (error != null) {
+                showSnackBar("카카오톡 공유 실패")
+            } else if (result != null) {
+                showSnackBar("카카오톡 공유 성공! ${result.intent}")
+                startActivity(context, result.intent, null)
+
+                // 카카오톡 공유에 성공했지만 아래 경고 메시지가 존재할 경우 일부 컨텐츠가 정상 동작하지 않을 수 있습니다.
+                Timber.d("Warning Msg: ${result.warningMsg}")
+                Timber.d("Argument Msg: ${result.argumentMsg}")
+            }
+        }
+    } else {
+        // 카카오톡 미설치: 웹 공유 사용 권장
+        val shareUrl = WebSharerClient.instance.makeDefaultUrl(codeMessage)
+
+        // CustomTabs로 웹 브라우저 열기
+        // 1. CustomTabServiceConnection 지원 부라우저 열기
+        // Chrome, 삼성 인터넷 등
+        try {
+            KakaoCustomTabsClient.openWithDefault(context, shareUrl)
+        } catch (e: UnsupportedOperationException) {
+            Timber.d("Web share error: ${e.message}")
+        }
+
+        // 2. CustomTabsServiceConnection 미지원 브라우저 열기
+        // 다음, 네이버 등
+        try {
+            KakaoCustomTabsClient.open(context, shareUrl)
+        } catch (e: ActivityNotFoundException) {
+            // 디바이스에 설치된 인터넷 브라우저 없을 때
+            showSnackBar("인터넷 브라우저를 설치해 주세요.")
         }
     }
 }
