135649e4b44faaf1b9f6429cf1bc855d6c4e9d61
윤가희 <yunkh2005@naver.com>
2024-11-07 15:33:54 +0900

    feat: stompHandler 완료

--- backend/family-service/src/main/java/com/familring/familyservice/config/websocket/StompHandler.java
+++ backend/family-service/src/main/java/com/familring/familyservice/config/websocket/StompHandler.java
@@ -26,58 +26,72 @@ public class StompHandler implements ChannelInterceptor {
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        StompCommand command = accessor.getCommand();
 
-        handleMessage(accessor.getCommand(), accessor, message.getHeaders());
+        // CONNECT 요청일 때, 헤더에서 X-User-ID를 추출해 세션에 저장
+        if (StompCommand.CONNECT.equals(command)) {
+            String userIdHeader = accessor.getFirstNativeHeader("X-User-ID");
+            log.info("WebSocket 연결 시 X-User-ID 헤더: {}", userIdHeader);
+
+            if (userIdHeader == null) {
+                log.error("X-User-ID 헤더가 누락되었습니다.");
+                throw new IllegalArgumentException("X-User-ID 헤더가 필요합니다.");
+            }
+
+            Long userId = Long.valueOf(userIdHeader);
+            accessor.getSessionAttributes().put("userId", userId); // 세션에 저장
+        }
+
+        // CONNECT 외의 다른 요청일 때 세션에서 userId를 가져옴
+        else {
+            Long userId = (Long) accessor.getSessionAttributes().get("userId");
+            if (userId == null) {
+                log.error("X-User-ID 세션 정보가 누락되었습니다.");
+                throw new IllegalArgumentException("X-User-ID 세션 정보가 필요합니다.");
+            }
+            log.info("세션에서 가져온 X-User-ID: {}", userId);
+        }
 
         return message;
     }
 
-    private void handleMessage(StompCommand command, StompHeaderAccessor accessor, MessageHeaders headers) {
+    private void handleMessage(StompCommand command, StompHeaderAccessor accessor, MessageHeaders headers, Long userId) {
         switch (command) {
             case CONNECT:
-                log.debug("[Handler - handleMessage] CONNECT");
+                log.info("[Handler - handleMessage] CONNECT, userId: {}", userId);
                 break;
             case SUBSCRIBE:
-                log.debug("[Handler - handleMessage] SUBSCRIBE");
-                Long roomId = connectToChatRoom(accessor, headers);
-                log.debug("[Handler - handleMessage] 채팅방 구독한 채팅 방 = {}", roomId);
+                log.info("[Handler - handleMessage] SUBSCRIBE");
+                Long roomId = connectToChatRoom(headers, userId);
+                log.info("[Handler - handleMessage] 채팅방 구독한 채팅 방 = {}", roomId);
                 break;
             case SEND:
-                log.debug("[Handler - handleMessage] SEND");
-                log.debug("[Handler - handleMessage] 메세지 맵핑 주소 = {}", accessor.getDestination());
+                log.info("[Handler - handleMessage] SEND");
+                log.info("[Handler - handleMessage] 메세지 맵핑 주소 = {}", accessor.getDestination());
                 break;
             case DISCONNECT:
-                log.debug("[Handler - handleMessage] DISCONNECT");
+                log.info("[Handler - handleMessage] DISCONNECT");
                 break;
         }
     }
 
-    private Long connectToChatRoom(StompHeaderAccessor accessor, MessageHeaders headers) {
-        log.debug("[Handler - connectToChatRoom] 시작");
+    private Long connectToChatRoom(MessageHeaders headers, Long userId) {
         Long roomId = getChatRoomNo(headers);
-        log.debug("[Handler - connectToChatRoom] 채팅방 번호 = {}", roomId);
-        Long userId = Long.valueOf(accessor.getFirstNativeHeader("X-User-ID"));
-        log.debug("[Handler - connectToChatRoom] userId = {}", userId);
+        log.info("[Handler - connectToChatRoom] 채팅방 번호 = {}", roomId);
+
         UserInfoResponse user = userServiceFeignClient.getUser(userId).getData();
-        log.debug("[Handler - connectToChatRoom] 회원 이름 = {}", user.getUserNickname());
+        log.info("[Handler - connectToChatRoom] 회원 이름 = {}", user.getUserNickname());
+
         chatService.connectChatRoom(roomId, userId);
-        log.debug("[Handler - connectToChatRoom] connectChatRoom() 연결 완료");
+        log.info("[Handler - connectToChatRoom] connectChatRoom() 연결 완료");
 
         return roomId;
     }
 
-    private String getRoomId(String destination) {
-        int lastIndex = destination.lastIndexOf('/');
-        if (lastIndex != -1) {
-            return destination.substring(lastIndex + 1);
-        } else {
-            return null;
-        }
-    }
-
     private Long getChatRoomNo(MessageHeaders headers) {
-        String roomId = getRoomId(Optional.ofNullable((String)
-                headers.get("simpDestination")).orElse("InvalidRoomId"));
-        return Long.valueOf(roomId);
+        String destination = Optional.ofNullable((String) headers.get("simpDestination")).orElse("InvalidRoomId");
+        int lastIndex = destination.lastIndexOf('/');
+        String roomIdStr = lastIndex != -1 ? destination.substring(lastIndex + 1) : "0";
+        return Long.valueOf(roomIdStr);
     }
 }
--- backend/family-service/src/main/java/com/familring/familyservice/config/websocket/WebSocketConfig.java
+++ backend/family-service/src/main/java/com/familring/familyservice/config/websocket/WebSocketConfig.java
@@ -23,12 +23,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 클라이언트에서 메시지를 전송할 때 사용하는 경로
         registry.setApplicationDestinationPrefixes("/send");
+        // 구독용 경로 설정
         registry.enableSimpleBroker("/room");
     }
 
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
+        // StompHandler로 메시지 가로채기
         registration.interceptors(stompHandler);
     }
 }
