2bc0cff59591827ad6889b42414d2a8d0302ec9c
김민경 <org9899@naver.com>
2024-11-08 16:42:32 +0900

    fix : 관심사 관리 dto 수정

--- backend/interest-service/src/main/java/com/familring/interestservice/domain/InterestAnswer.java
+++ backend/interest-service/src/main/java/com/familring/interestservice/domain/InterestAnswer.java
@@ -1,2 +1,37 @@
-package com.familring.interestservice.domain;public class InterestAnswer {
+package com.familring.interestservice.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicInsert
+@Table(name = "interest_answer")
+public class InterestAnswer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="interest_answer_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "interest_select_id")
+    private InterestSelect interestSelect;
+
+    @Column(name="user_id")
+    private Long userId;
+
+    @Column(name="family_id")
+    private Long familyId;
+
+    @Column(name="interest_answer_content")
+    private String content;
+
 }
--- backend/interest-service/src/main/java/com/familring/interestservice/domain/InterestSelect.java
+++ backend/interest-service/src/main/java/com/familring/interestservice/domain/InterestSelect.java
@@ -1,2 +1,24 @@
-package com.familring.interestservice.domain;public class InterestSelect {
+package com.familring.interestservice.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicInsert
+@Table(name = "interest_select")
+public class InterestSelect {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="interest_select_id")
+    private Long id;
+
 }
