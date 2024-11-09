002ccc8c35e57cb605ec8c544c35a080d02b806a
김민경 <org9899@naver.com>
2024-11-09 18:16:11 +0900

    feat : user, family client service 추가

---
+++ backend/interest-service/src/main/java/com/familring/interestservice/service/client/UserServiceFeignClient.java
@@ -0,0 +1,15 @@
+package com.familring.interestservice.service.client;
+
+import com.familring.common_module.dto.BaseResponse;
+import com.familring.questionservice.dto.client.UserInfoResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name = "user-service")
+public interface UserServiceFeignClient {
+    @PostMapping("/client/users")
+    BaseResponse<List<UserInfoResponse>> getAllUser(@RequestBody List<Long> userIds);
+}
