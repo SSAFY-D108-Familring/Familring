46e646a56a328a1b69ae7344a5bf82b203c7efe2
김민경 <org9899@naver.com>
2024-11-09 21:45:21 +0900

    feat : 관심사 가족 답변 목록 조회 dto 생성

---
+++ backend/interest-service/src/main/java/com/familring/interestservice/dto/response/InterestAnswerItem.java
@@ -0,0 +1,19 @@
+package com.familring.interestservice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class InterestAnswerItem {
+
+    private Long userId;
+    private String userNickname;
+    private String userZodiacSign;
+    private String content; // 어떤 관심사인지
+
+}
