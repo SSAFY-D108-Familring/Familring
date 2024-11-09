84ec2e7b484443c0fff5eeb12103053e554d2d88
김민경 <org9899@naver.com>
2024-11-10 02:03:11 +0900

    feat : 관심사 체험 인증 설정할 때 오늘 이후가 아닐때, 이미 선정되어 있을 때 exception 처리

--- backend/interest-service/src/main/java/com/familring/interestservice/exception/AlreadyExistInterestMissionEndDateException.java
+++ backend/interest-service/src/main/java/com/familring/interestservice/exception/AlreadyExistInterestMissionEndDateException.java
@@ -1,2 +1,10 @@
-package com.familring.interestservice.exception;public class AlreadyExistInterestMissionEndDateException {
+package com.familring.interestservice.exception;
+
+import com.familring.interestservice.exception.base.ApplicationException;
+import com.familring.interestservice.exception.constant.ErrorDetail;
+
+public class AlreadyExistInterestMissionEndDateException extends ApplicationException {
+    public AlreadyExistInterestMissionEndDateException() {
+        super(ErrorDetail.EXIST_INTEREST_MISSION_END_DATE);
+    }
 }
--- backend/interest-service/src/main/java/com/familring/interestservice/exception/InvalidInterestMissionEndDateException.java
+++ backend/interest-service/src/main/java/com/familring/interestservice/exception/InvalidInterestMissionEndDateException.java
@@ -1,2 +1,10 @@
-package com.familring.interestservice.exception;public class InvalidInterestMissionEndDateException {
+package com.familring.interestservice.exception;
+
+import com.familring.interestservice.exception.base.ApplicationException;
+import com.familring.interestservice.exception.constant.ErrorDetail;
+
+public class InvalidInterestMissionEndDateException extends ApplicationException {
+    public InvalidInterestMissionEndDateException() {
+        super(ErrorDetail.INVALID_INTEREST_MISSION_END_DATE);
+    }
 }
