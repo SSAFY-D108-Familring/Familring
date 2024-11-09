0439192fb2db2a537b36090fb359bdc0360b19d8
김민경 <org9899@naver.com>
2024-11-09 20:41:13 +0900

    feat : 가장 최근 관심사 찾기 함수 추가 (repository)

--- backend/interest-service/src/main/java/com/familring/interestservice/repository/InterestRepository.java
+++ backend/interest-service/src/main/java/com/familring/interestservice/repository/InterestRepository.java
@@ -11,4 +11,7 @@ public interface InterestRepository extends JpaRepository<Interest, Integer> {
 
     Optional<Interest> findByIdAndFamilyId(Long id, Long familyId);
 
+    // 가족에 가장 최근 관심사 찾기
+    Optional<Interest> findFirstByFamilyId(Long familyId);
+
 }
