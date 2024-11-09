1c320384745275e2de35f510aee66f2826fc6477
김민경 <org9899@naver.com>
2024-11-09 21:46:17 +0900

    feat : userID로 user 정보 조회 (feignclient 추가)

--- backend/interest-service/src/main/java/com/familring/interestservice/service/client/UserServiceFeignClient.java
+++ backend/interest-service/src/main/java/com/familring/interestservice/service/client/UserServiceFeignClient.java
@@ -3,6 +3,10 @@ package com.familring.interestservice.service.client;
 import com.familring.common_module.dto.BaseResponse;
 import com.familring.interestservice.dto.client.UserInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,4 +16,7 @@ import java.util.List;
 public interface UserServiceFeignClient {
     @PostMapping("/client/users")
     BaseResponse<List<UserInfoResponse>> getAllUser(@RequestBody List<Long> userIds);
+
+    @GetMapping("/client/users/{userId}")
+    BaseResponse<UserInfoResponse> getUser(@PathVariable Long userId);
 }
