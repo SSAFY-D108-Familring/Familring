5742db357505f56549da2d264bda71ab6e7730da
김민경 <org9899@naver.com>
2024-11-09 20:16:36 +0900

    feat : 관심사 찾기 함수 추가

--- backend/interest-service/src/main/java/com/familring/interestservice/repository/InterestAnswerRepository.java
+++ backend/interest-service/src/main/java/com/familring/interestservice/repository/InterestAnswerRepository.java
@@ -1,9 +1,15 @@
 package com.familring.interestservice.repository;
 
+import com.familring.interestservice.domain.Interest;
 import com.familring.interestservice.domain.InterestAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InterestAnswerRepository extends JpaRepository<InterestAnswer, Integer> {
+
+    Optional<InterestAnswer> findByInterest(Interest interest);
+
 }
