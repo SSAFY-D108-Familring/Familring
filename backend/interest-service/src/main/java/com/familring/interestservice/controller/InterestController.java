742cf85704be6298793516e6566acf011a27447a
김민경 <org9899@naver.com>
2024-11-09 19:36:44 +0900

    feat : 관심사 작성 api 생성

--- backend/interest-service/src/main/java/com/familring/interestservice/controller/InterestController.java
+++ backend/interest-service/src/main/java/com/familring/interestservice/controller/InterestController.java
@@ -1,9 +1,27 @@
 package com.familring.interestservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.familring.common_module.dto.BaseResponse;
+import com.familring.interestservice.dto.request.InterestAnswerCreateRequest;
+import com.familring.interestservice.service.InterestService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/interests")
+@RequiredArgsConstructor
 public class InterestController {
+
+    private final InterestService interestService;
+
+    @PostMapping("/answers")
+    @Operation(summary = "관심사 답변 작성", description = "관심사 답변 작성 (내용만 입력)")
+    public ResponseEntity<BaseResponse<Void>> createInterestAnswer(@Parameter(hidden = true) @RequestHeader("X-User-ID") Long userId, @RequestBody InterestAnswerCreateRequest interestAnswerCreateRequest) {
+        interestService.createInterestAnswer(userId, interestAnswerCreateRequest);
+        return ResponseEntity.ok(BaseResponse.create(HttpStatus.OK.value(), "관심사 답변 작성에 성공했습니다."));
+    }
+
 }
--- backend/interest-service/src/main/java/com/familring/interestservice/service/InterestService.java
+++ backend/interest-service/src/main/java/com/familring/interestservice/service/InterestService.java
@@ -1,5 +1,14 @@
 package com.familring.interestservice.service;
 
+import com.familring.interestservice.domain.Interest;
+import com.familring.interestservice.domain.InterestAnswer;
+import com.familring.interestservice.dto.client.Family;
+import com.familring.interestservice.dto.request.InterestAnswerCreateRequest;
+import com.familring.interestservice.repository.InterestAnswerRepository;
+import com.familring.interestservice.repository.InterestMissionRepository;
+import com.familring.interestservice.repository.InterestRepository;
+import com.familring.interestservice.service.client.FamilyServiceFeignClient;
+import com.familring.interestservice.service.client.UserServiceFeignClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,6 +18,38 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class InterestService {
 
+    private final InterestRepository interestRepository;
+    private final InterestAnswerRepository interestAnswerRepository;
+    private final InterestMissionRepository interestMissionRepository;
+    private final FamilyServiceFeignClient familyServiceFeignClient;
+    private final UserServiceFeignClient userServiceFeignClient;
 
+    // 관심사 답변 작성
+    public void createInterestAnswer(Long userId, InterestAnswerCreateRequest interestAnswerCreateRequest) {
+
+        // 가족 조회
+        Family family = familyServiceFeignClient.getFamilyInfo(userId).getData();
+        Long familyId = family.getFamilyId();
+
+        // 관심사
+        Interest interest = Interest
+                .builder()
+                .familyId(familyId)
+                .build();
+
+        interestRepository.save(interest);
+
+        // 관심사 답변
+        InterestAnswer interestAnswer = InterestAnswer
+                .builder()
+                .familyId(familyId)
+                .userId(userId)
+                .interest(interest)
+                .content(interestAnswerCreateRequest.getContent())
+                .build();
+
+        interestAnswerRepository.save(interestAnswer);
+
+    }
 
 }
