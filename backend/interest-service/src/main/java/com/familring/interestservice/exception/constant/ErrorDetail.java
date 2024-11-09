59fdcd990910353ac65fba6d3438de3bd1cc8dca
김민경 <org9899@naver.com>
2024-11-09 20:16:07 +0900

    feat : errorDetail 관심사 답변 못찾았을 때 추가

--- backend/interest-service/src/main/java/com/familring/interestservice/exception/constant/ErrorDetail.java
+++ backend/interest-service/src/main/java/com/familring/interestservice/exception/constant/ErrorDetail.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorDetail {
 
-    NOT_FOUND_INTEREST("I0001", HttpStatus.NOT_FOUND, "관심사를 찾지 못했습니다.");
+    NOT_FOUND_INTEREST("I0001", HttpStatus.NOT_FOUND, "관심사를 찾지 못했습니다."),
+    NOT_FOUND_INTEREST_ANSWER("I0002", HttpStatus.NOT_FOUND, "작성한 관심사 답변을 찾지 못했습니다.");
 
     private final String errorCode;
     private final HttpStatus httpStatus;
