b9cc7346942102dbdc4b937cbe89ec1c8d20b333
최승준 <tmdwns63@gmail.com>
2024-11-11 09:58:40 +0900

    chore: mr 받은 후 코드 수정

--- android/presentation/src/main/java/com/familring/presentation/screen/gallery/AlbumScreen.kt
+++ android/presentation/src/main/java/com/familring/presentation/screen/gallery/AlbumScreen.kt
@@ -378,6 +378,7 @@ fun AlbumScreen(
                         .background(color = Black.copy(alpha = 0.5f)),
                 contentAlignment = Alignment.Center,
             ) {
+                //로딩추가
                 CircularProgressIndicator(color = Green02)
             }
         }
