46b7da7ab4f572123be909774682345bcce751e1
최승준 <tmdwns63@gmail.com>
2024-11-12 18:00:18 +0900

    chore: 로딩화면 수정 & 로그인 로딩 수정 중..

--- android/presentation/src/main/java/com/familring/presentation/screen/login/LoginEvent.kt
+++ android/presentation/src/main/java/com/familring/presentation/screen/login/LoginEvent.kt
@@ -1,6 +1,17 @@
 package com.familring.presentation.screen.login
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
+
+@Stable
 sealed interface LoginEvent {
+    @Immutable
+    data object None : LoginEvent
+
+    @Immutable
+    data object Loading : LoginEvent
+
+    @Immutable
     data object LoginSuccess : LoginEvent
 
     data class Error(
