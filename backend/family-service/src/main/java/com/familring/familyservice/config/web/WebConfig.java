453f9fc5335b4033c0acd4edb2a19844cda64b7e
윤가희 <yunkh2005@naver.com>
2024-11-12 23:41:08 +0900

    fix: family-service 내/family 정적 리소스 경로 무시

--- backend/family-service/src/main/java/com/familring/familyservice/config/web/WebConfig.java
+++ backend/family-service/src/main/java/com/familring/familyservice/config/web/WebConfig.java
@@ -3,11 +3,14 @@ package com.familring.familyservice.config.web;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
+import java.io.IOException;
 import java.util.List;
 
 @Configuration
@@ -37,5 +40,17 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/")
                 .setCachePeriod(3600);
+
+        // API 경로를 정적 리소스에서 제외
+        registry.addResourceHandler("/family/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        return null; // 정적 리소스 경로에 대한 모든 요청을 무시
+                    }
+                });
     }
 }
