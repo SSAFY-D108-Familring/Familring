b6c2189d5e78e2e6e487b3c6b03049e6283e0b15
HYUNJIN LEE <dlguswls456@naver.com>
2024-11-12 14:20:21 +0900

    feat(#S11P31D108-374) : 관심사 선정 후 status 재호출

--- android/presentation/src/main/java/com/familring/presentation/screen/interest/InterestViewModel.kt
+++ android/presentation/src/main/java/com/familring/presentation/screen/interest/InterestViewModel.kt
@@ -165,7 +165,7 @@ class InterestViewModel
                 interestRepository.selectInterest().collect { result ->
                     when (result) {
                         is ApiResponse.Success -> {
-                            //
+                            getInterestStatus()
                         }
 
                         is ApiResponse.Error -> {
