7c0282998fe48804127dbe104044f36167aa06a8
김민경 <org9899@naver.com>
2024-11-13 17:12:04 +0900

    feat : 알림 client 추가

--- backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/service/client/NotificationServiceFeignClient.java
+++ backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/service/client/NotificationServiceFeignClient.java
@@ -1,7 +1,7 @@
-package com.familring.questionservice.service.client;
+package com.familring.timecapsuleservice.service.client;
 
 import com.familring.common_module.dto.BaseResponse;
-import com.familring.questionservice.dto.client.NotificationRequest;
+import com.familring.timecapsuleservice.dto.client.NotificationRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
