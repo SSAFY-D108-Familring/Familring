8ef296de59955a05e3ef5df72053b9d1720f9af8
HYUNJIN LEE <dlguswls456@naver.com>
2024-11-13 16:32:24 +0900

    feat(#S11P31D108-368) : 튜토리얼 컴포넌트 생성

--- android/presentation/src/main/java/com/familring/presentation/component/TutorialScreen.kt
+++ android/presentation/src/main/java/com/familring/presentation/component/TutorialScreen.kt
@@ -1,25 +1,18 @@
-package com.familring.presentation.screen.interest.tutorial
+package com.familring.presentation.component
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -28,25 +21,21 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.familring.presentation.R
 import com.familring.presentation.theme.Gray01
-import com.familring.presentation.theme.Gray03
-import com.familring.presentation.theme.Green02
 import com.familring.presentation.theme.Typography
 
 @Composable
-fun InterestTutorialScreen(modifier: Modifier = Modifier) {
-    val pagerState = rememberPagerState(pageCount = { 4 })
-    val tutorialImages =
-        listOf(
-            R.drawable.img_tutorial_interest_first,
-            R.drawable.img_tutorial_interest_second,
-            R.drawable.img_tutorial_interest_third,
-            R.drawable.img_tutorial_interest_fourth,
-        )
+fun TutorialScreen(
+    modifier: Modifier = Modifier,
+    imageLists: List<Int>,
+    title: String = "",
+    subTitle: String = "",
+) {
+    val pagerState = rememberPagerState(pageCount = { imageLists.size })
     val maxHeight =
         with(
             LocalDensity.current,
         ) {
-            tutorialImages.maxOfOrNull {
+            imageLists.maxOfOrNull {
                 painterResource(id = it).intrinsicSize.height.toDp()
             }
         }
@@ -58,15 +47,13 @@ fun InterestTutorialScreen(modifier: Modifier = Modifier) {
                 .padding(20.dp),
     ) {
         Text(
-            text = "관심사 공유 미리보기  \uD83D\uDD0D",
+            text = title,
             style = Typography.titleLarge.copy(fontSize = 25.sp),
         )
         Spacer(modifier = Modifier.height(20.dp))
         Text(
             modifier = Modifier.fillMaxWidth(),
-            text =
-                "가족들의 최근 관심사를 알아보고\n" +
-                    "체험한 후 인증하며 가까워질 수 있어요 !",
+            text = subTitle,
             style =
                 Typography.displayLarge.copy(
                     color = Gray01,
@@ -87,37 +74,31 @@ fun InterestTutorialScreen(modifier: Modifier = Modifier) {
                 contentAlignment = Alignment.Center,
             ) {
                 Image(
-                    painter = painterResource(id = tutorialImages[page]),
-                    contentDescription = "img_tutorial_interest_first",
+                    painter = painterResource(id = imageLists[page]),
+                    contentDescription = "img_tutorial",
                 )
             }
         }
-        Row(
-            modifier =
-                Modifier
-                    .wrapContentHeight()
-                    .fillMaxWidth()
-                    .padding(top = 20.dp),
-            horizontalArrangement = Arrangement.Center,
-        ) {
-            repeat(pagerState.pageCount) { iteration ->
-                val color =
-                    if (pagerState.currentPage == iteration) Green02 else Gray03
-                Box(
-                    modifier =
-                        Modifier
-                            .padding(4.dp)
-                            .clip(CircleShape)
-                            .size(15.dp)
-                            .background(color),
-                )
-            }
-        }
+        PagerIndicator(
+            pagerState = pagerState,
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun InterestTutorialScreenPreview() {
-    InterestTutorialScreen()
+    TutorialScreen(
+        imageLists =
+            listOf(
+                R.drawable.img_tutorial_interest_first,
+                R.drawable.img_tutorial_interest_second,
+                R.drawable.img_tutorial_interest_third,
+                R.drawable.img_tutorial_interest_fourth,
+            ),
+        title = "관심사 공유 미리보기  \uD83D\uDD0D",
+        subTitle =
+            "가족들의 최근 관심사를 알아보고\n" +
+                "체험한 후 인증하며 가까워질 수 있어요 !",
+    )
 }
