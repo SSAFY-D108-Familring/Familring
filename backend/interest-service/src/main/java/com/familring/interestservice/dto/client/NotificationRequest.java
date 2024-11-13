f0d1707f065df68765d29f6ddbd7f4aa8c2ddb17
김민경 <org9899@naver.com>
2024-11-14 01:11:34 +0900

    feat : notification dto 추가 (feign client)

---
+++ backend/interest-service/src/main/java/com/familring/interestservice/dto/client/NotificationRequest.java
@@ -0,0 +1,21 @@
+package com.familring.interestservice.dto.client;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationRequest {
+    private NotificationType notificationType;      // 알림 발생지
+    private List<Long> receiverUserIds; // 알림 받는 사람들 Id
+    private Long senderUserId;          // 알림 전송하는 사람 Id
+    private String destinationId;       // 알림으로 이동할 Id
+    private String title;               // 알림 title
+    private String message;             // 알림 보낼 message
+}
