e0808f8c59fd2244c92b44adee9f004fc0f9fa8c
김민경 <org9899@naver.com>
2024-11-13 17:11:39 +0900

    feat : 알림 dto 생성 (feign client)

--- backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/dto/client/NotificationRequest.java
+++ backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/dto/client/NotificationRequest.java
@@ -1,11 +1,10 @@
-package com.familring.questionservice.dto.client;
+package com.familring.timecapsuleservice.dto.client;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
--- backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/dto/client/NotificationType.java
+++ backend/timecapsule-service/src/main/java/com/familring/timecapsuleservice/dto/client/NotificationType.java
@@ -1,4 +1,4 @@
-package com.familring.questionservice.dto.client;
+package com.familring.timecapsuleservice.dto.client;
 
 public enum NotificationType {
     KNOCK,              // 랜덤 질문 노크
