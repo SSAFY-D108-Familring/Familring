2aff9052cfc5cba1186d180e53a4d1141aee3249
HYUNJIN LEE <dlguswls456@naver.com>
2024-11-14 14:06:48 +0900

    feat(#S11P31D108-368) : 채팅 튜토리얼 멘트 수정

--- android/presentation/src/main/java/com/familring/presentation/screen/calendar/CalendarTab.kt
+++ android/presentation/src/main/java/com/familring/presentation/screen/calendar/CalendarTab.kt
@@ -27,6 +27,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
