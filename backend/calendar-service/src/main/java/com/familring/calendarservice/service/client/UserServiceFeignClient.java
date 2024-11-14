df45cc2cf9b15c97f5840279a2144ad52c8f4fb1
권오윤 <rdd2033@naver.com>
2024-11-14 13:37:17 +0900

    feat: 일정 등록, 수정, 삭제시 알림 전송

--- backend/calendar-service/src/main/java/com/familring/calendarservice/service/client/UserServiceFeignClient.java
+++ backend/calendar-service/src/main/java/com/familring/calendarservice/service/client/UserServiceFeignClient.java
@@ -3,6 +3,10 @@ package com.familring.calendarservice.service.client;
 import com.familring.calendarservice.dto.client.UserInfoResponse;
 import com.familring.common_module.dto.BaseResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,4 +16,7 @@ import java.util.List;
 public interface UserServiceFeignClient {
     @PostMapping("/client/users")
     BaseResponse<List<UserInfoResponse>> getAllUser(@RequestBody List<Long> userIds);
+
+    @GetMapping("/client/{userId}")
+    BaseResponse<UserInfoResponse> getUser(@PathVariable Long userId);
 }
